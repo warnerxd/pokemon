f00c5c521bf4d83eb022a0c6973ae58b70298633
warnerxd <warnerxd@gmail.com>
2024-10-18 12:43:29 -0500

    pokemon2

---
+++ Pokemon.py
@@ -0,0 +1,12 @@
+class Pokemon:
+
+    def __init__(self,nombre,ataque,defenza,ataque_e,defenza_e,velocidad):
+        self.nombre=nombre
+        self.ataque=ataque
+        self.defenza=defenza
+        self.ataque_e=ataque_e
+        self.defenza_e=defenza_e
+        self.velocidad=velocidad
+        
+
+
